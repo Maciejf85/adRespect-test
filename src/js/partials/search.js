973467b790835e1ccfdd3dd4d8a29edde07b27a2
Maciej Fiałkowski <Fialek85@gmail.com>
2023-08-18 18:35:19 +0200

    feat(homepage): add header styles with functionality

--- src/js/partials/search.js
+++ src/js/partials/search.js
@@ -1,10 +1,12 @@
 // eslint-disable-next-line import/prefer-default-export
 export const search = () => {
   const searchIcon = document.querySelector('.js-search');
+  const searchBar = document.querySelector('.js-searchBar');
 
-  if (searchIcon) {
+  if (searchIcon && searchBar) {
     searchIcon.addEventListener('click', function () {
       this.classList.toggle('active');
+      searchBar.classList.toggle('active');
     });
   }
 };
