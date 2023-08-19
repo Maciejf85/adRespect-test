01ce847ef0c6b4db95409047246be12687867b9d
Maciej Fiałkowski <Fialek85@gmail.com>
2023-08-19 16:08:58 +0200

    feat(homepage): add offer section with desktop and mobile styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
+import '@iconfu/svg-inject';
 import { search } from './partials/search';
 import { swiperJs } from './partials/swiper';
 
 window.addEventListener('load', () => {
+  SVGInject(document.querySelectorAll('img.injectable'));
   search();
   swiperJs();
 });
